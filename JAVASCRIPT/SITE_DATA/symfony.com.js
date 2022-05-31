d79be9087643d1a12116566b35e44d57d905df10
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-05-31 14:02:22 +0700

    change symfony docs to new

---
+++ JAVASCRIPT/SITE_DATA/symfony.com.js
@@ -0,0 +1,28 @@
+DATA = [
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'body',
+        CLASS_LIST_BY_SPACE: 'UPPER w3-white w3-text-black MARGIN_TOP_0 PADDING_TOP_0'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'code,pre,.codeblock,',
+        CLASS_LIST_BY_SPACE: 'CONG_CODE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '#ukraine-message,.doc-header-container,.doc-sidebar-highlight,.top-horizontal-highlights,#sln,.doc-content-embedded-sidebar,.doc-license,footer,',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.doc-content',
+        CLASS_LIST_BY_SPACE: 'PADDING_0'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.ui-prose',
+        CSS_TEXT: 'max-width: 100%;'
+    },
+    {EMPTY_LINE: 'EMPTY_LINE'}
+];
