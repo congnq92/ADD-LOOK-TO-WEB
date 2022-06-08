f9e5d8d25c9c8391dd0228189eeedda25a55d6a4
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-08 17:20:23 +0700

    add some to angular.io.js

--- JAVASCRIPT/SITE_DATA/angular.io.js
+++ JAVASCRIPT/SITE_DATA/angular.io.js
@@ -11,13 +11,28 @@ DATA = [
     },
     {
         TYPE: 'ADD_CLASS',
-        SELECTOR_CSS_LIST_BY_COMMA: 'aio-notification,footer',
+        SELECTOR_CSS_LIST_BY_COMMA: '.mat-toolbar-row.notification-container,footer',
         CLASS_LIST_BY_SPACE: 'HIDE'
     },
     {
         TYPE: 'ADD_CSS',
         SELECTOR_CSS_LIST_BY_COMMA: '.mat-toolbar-row',
         CSS_TEXT: 'height: 1.5em !important;'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.mat-toolbar-multiple-rows',
+        CSS_TEXT: 'min-height: 0 !important;'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'mat-sidenav-container.sidenav-container mat-sidenav.sidenav',
+        CSS_TEXT: 'top: 20px !important;'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'p,ol,ul,li,input,a,aio-nav-menu aio-nav-item',
+        CSS_TEXT: 'font-size: 1em !important; line-height: 1.3em !important;'
     },
     {EMPTY_LINE: 'EMPTY_LINE'}
 ];
