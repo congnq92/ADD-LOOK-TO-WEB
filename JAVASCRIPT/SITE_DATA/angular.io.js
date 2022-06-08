a2d13ae08f3a901ffb1c0b49dfba300f88906160
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-08 17:07:23 +0700

    add angular.io.js

---
+++ JAVASCRIPT/SITE_DATA/angular.io.js
@@ -0,0 +1,23 @@
+DATA = [
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'body',
+        CLASS_LIST_BY_SPACE: 'UPPER w3-white w3-text-black'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'code,pre',
+        CLASS_LIST_BY_SPACE: 'CONG_CODE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'aio-notification,footer',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.mat-toolbar-row',
+        CSS_TEXT: 'height: 1.5em !important;'
+    },
+    {EMPTY_LINE: 'EMPTY_LINE'}
+];
