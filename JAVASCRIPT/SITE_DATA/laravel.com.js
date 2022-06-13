c32fc8519ec747e69d88a2414ebead27a8e1a2ec
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-13 17:29:54 +0700

    add laravel.js

---
+++ JAVASCRIPT/SITE_DATA/laravel.com.js
@@ -0,0 +1,33 @@
+DATA = [
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'body',
+        CLASS_LIST_BY_SPACE: 'UPPER w3-white w3-text-black'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'code,pre,.torchlight,#collection-method-list',
+        CLASS_LIST_BY_SPACE: 'CONG_CODE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '#carbonads,footer,blockquote:first-child',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.max-w-screen-lg',
+        CSS_TEXT: 'padding: 0 30px!important; max-width: 100% !important;'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.docs_main',
+        CSS_TEXT: 'max-width: 100% !important;'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '#main-content',
+        CLASS_LIST_BY_SPACE: 'TEXT_SIZE_130'
+    },
+    {EMPTY_LINE: 'EMPTY_LINE'}
+];
