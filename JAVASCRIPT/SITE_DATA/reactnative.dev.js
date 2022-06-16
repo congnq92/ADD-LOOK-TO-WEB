bfe4ef9f5e99e0b9ea5dfd9e917c0bd7b61d1316
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-16 17:32:42 +0700

    add react native

---
+++ JAVASCRIPT/SITE_DATA/reactnative.dev.js
@@ -0,0 +1,34 @@
+DATA = [
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'body',
+        CLASS_LIST_BY_SPACE: 'UPPER w3-white w3-text-black'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'code,pre',
+        CLASS_LIST_BY_SPACE: 'CONG_CODE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'footer',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CLASS_FIND',
+        ELEMENT: 'div',
+        CLASS_PART_LIST_BY_COMMA: 'announcementBar',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.table-of-contents__link--active a',
+        CSS_TEXT: 'border: #2196F3 1px solid !important; border-top-left-radius: 7px !important; border-bottom-left-radius: 7px !important; margin-left: -5px; border-left: #2196F3 7px solid !important;'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.table-of-contents__link--active a',
+        CSS_TEXT: 'color: #4CAF50 !important;'
+    },
+    {EMPTY_LINE: 'EMPTY_LINE'}
+];
