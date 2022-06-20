822ff83ad492de9a6cbbd498894219d015a09e31
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-20 17:12:39 +0700

    test menu

--- JAVASCRIPT/SITE_DATA/reactnative.dev.js
+++ JAVASCRIPT/SITE_DATA/reactnative.dev.js
@@ -22,16 +22,17 @@ DATA = [
     },
     // LEVEL 0
     {
-        TYPE: 'ADD_CSS',
-        SELECTOR_CSS_LIST_BY_COMMA: '.menu__list-item-collapsible>.menu__link--active',
-        CSS_TEXT: 'font-weight: 600 !important; border: #2196F3 1px solid !important;border-top-left-radius: 0px !important; border-bottom-left-radius: 0px !important; border-top-right-radius: 7px !important; border-bottom-right-radius: 7px !important; border-right: #2196F3 7px solid !important;'
+        TYPE: 'ADD_CLASS_TO_OBJECT',
+        SELECTOR_CSS: '.menu__list-item-collapsible>.menu__link--active',
+        CLASS_LIST_BY_SPACE: 'w3-border w3-border-blue',
+        PARENT_LEVEL: 2, // DEFAULT NO PARENT LEVEL
     },
-// LEVEL 2
-    {
-        TYPE: 'ADD_CSS',
-        SELECTOR_CSS_LIST_BY_COMMA: '.menu__link--active',
-        CSS_TEXT: 'font-weight: 600 !important; border: #2196F3 1px solid !important;border-top-left-radius: 0px !important; border-bottom-left-radius: 0px !important; border-top-right-radius: 7px !important; border-bottom-right-radius: 7px !important; border-right: #2196F3 7px solid !important;'
-    },
+    // LEVEL 2
+    // {
+    //     TYPE: 'ADD_CSS',
+    //     SELECTOR_CSS_LIST_BY_COMMA: '.menu__link--active',
+    //     CSS_TEXT: 'font-weight: 600 !important; border: #2196F3 1px solid !important;border-top-left-radius: 0px !important; border-bottom-left-radius: 0px !important; border-top-right-radius: 7px !important; border-bottom-right-radius: 7px !important; border-right: #2196F3 7px solid !important;'
+    // },
     {
         TYPE: 'ADD_CSS',
         SELECTOR_CSS_LIST_BY_COMMA: '.table-of-contents__link--active',
