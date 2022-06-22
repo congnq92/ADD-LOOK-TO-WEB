a7841deb3b58f4311eb4bd0c9253e744e826f9aa
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-22 16:43:14 +0700

    abcd

---
+++ JAVASCRIPT/SITE_DATA/lodash.com.js
@@ -0,0 +1,28 @@
+DATA = [
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'body',
+        CLASS_LIST_BY_SPACE: 'UPPER w3-white w3-text-black'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'code,pre,em',
+        CLASS_LIST_BY_SPACE: 'CONG_CODE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '#carbonads',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.header-group',
+        CSS_TEXT: 'height: 5px !important;'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '#version',
+        CSS_TEXT: 'padding: 0px !important; font-size: 1em !important; height: 20px !important;'
+    },
+    {EMPTY_LINE: 'EMPTY_LINE'}
+];
