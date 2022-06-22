d4907425830c764d9c6a29467f4bbbe82135d4a4
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-22 15:53:24 +0700

    react* bro*

--- JAVASCRIPT/SITE_DATA/reactnative.dev.js
+++ JAVASCRIPT/SITE_DATA/reactnative.dev.js
@@ -32,6 +32,11 @@ DATA = [
         CSS_TEXT: 'font-weight: 600 !important; border: #2196F3 1px solid !important;border-top-left-radius: 0px !important; border-bottom-left-radius: 0px !important; border-top-right-radius: 7px !important; border-bottom-right-radius: 7px !important; border-right: #2196F3 7px solid !important; color: #2196F3 !important; background: #fff !important;'
     },
     {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.table-of-contents',
+        CLASS_LIST_BY_SPACE: 'LINE_HEIGHT_200'
+    },
+    {
         TYPE: 'ADD_CSS',
         SELECTOR_CSS_LIST_BY_COMMA: '.table-of-contents__link--active',
         CSS_TEXT: 'border: #2196F3 1px solid !important; border-top-left-radius: 7px !important; border-bottom-left-radius: 7px !important; margin-left: -5px; border-left: #2196F3 7px solid !important; padding: 4px !important;'
