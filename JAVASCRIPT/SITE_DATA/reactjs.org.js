092648eebea30fedcdefedfd3e0539c6a9dcf9d7
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-23 10:53:59 +0700

    abcd

---
+++ JAVASCRIPT/SITE_DATA/reactjs.org.js
@@ -0,0 +1,18 @@
+DATA = [
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'body',
+        CLASS_LIST_BY_SPACE: 'UPPER w3-white w3-text-black'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'code,pre',
+        CLASS_LIST_BY_SPACE: 'CONG_CODE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'header:first-child,footer,article>div>div:last-child',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {EMPTY_LINE: 'EMPTY_LINE'}
+];
