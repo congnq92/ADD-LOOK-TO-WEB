3b67dd9291b706a08adcf8ec720e84d00c90bf57
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-06-23 11:02:00 +0700

    abcd

--- JAVASCRIPT/SITE_DATA/reactjs.org.js
+++ JAVASCRIPT/SITE_DATA/reactjs.org.js
@@ -11,8 +11,18 @@ DATA = [
     },
     {
         TYPE: 'ADD_CLASS',
-        SELECTOR_CSS_LIST_BY_COMMA: 'header:first-child,footer,article>div>div:last-child',
+        SELECTOR_CSS_LIST_BY_COMMA: 'header>div,footer,article>div>div:last-child',
         CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'nav li a[aria-current=\'page\']',
+        CSS_TEXT: 'border: #2196F3 1px solid !important; border-top-left-radius: 7px !important; border-bottom-left-radius: 7px !important; margin-left: -5px; border-left: #2196F3 7px solid !important; padding: 4px !important;'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'nav li a[aria-current=\'page\']',
+        CSS_TEXT: 'color: #4CAF50 !important;'
     },
     {EMPTY_LINE: 'EMPTY_LINE'}
 ];
