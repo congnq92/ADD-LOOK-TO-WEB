96b45f4c515b530e05ae673df56b7b0e0b6074e1
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-07-25 16:10:45 +0700

    add api-platform.com

---
+++ JAVASCRIPT/SITE_DATA/api-platform.com.js
@@ -0,0 +1,34 @@
+DATA = [
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'body',
+        CLASS_LIST_BY_SPACE: 'UPPER w3-white w3-text-black'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'code,pre,.gatsby-highlight,.language-text',
+        CLASS_LIST_BY_SPACE: 'CONG_CODE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.header,.header__additional-message,.footer',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'page',
+        CLASS_LIST_BY_SPACE: 'PADDING_TOP_0'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.page__docs',
+        CLASS_LIST_BY_SPACE: 'PADDING_0'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.docs__nav',
+        CSS_TEXT: 'width: 80% !important; background: #fff; box-shadow: none;'
+    },
+    //
+    {EMPTY_LINE: 'EMPTY_LINE'}
+];
--- JAVASCRIPT/SITE_DATA_OLD/api-platform.com.js
+++ JAVASCRIPT/SITE_DATA_OLD/api-platform.com.js
@@ -1,5 +1,11 @@
 const CSS = ".CONG_WORDS, h1, h2, h3, h4 {text-transform: uppercase;word-spacing: 4px;font-weight: 555;color:#000!important;background:#fff}.CONG_CODE .gatsby-highlight, .CONG_CODE .language-text {text-transform:none}";
-const CSS_API_PLATFORM_SITE = ".header__additional-message{display:none;}.header{display:none;}.page{padding-top:0 !important;}.page__docs{padding: 0 !important;}.footer{display:none;} .docs__nav{width: 80% !important; background: #fff; box-shadow: none;}";
+const CSS_API_PLATFORM_SITE = "" +
+    ".header__additional-message{display:none;}" +
+    ".header{display:none;}" +
+    ".page{padding-top:0 !important;}" +
+    ".page__docs{padding: 0 !important;}" +
+    ".footer{display:none;} " +
+    ".docs__nav{width: 80% !important; background: #fff; box-shadow: none;}";
 const STYLE = document.createElement("style");
 STYLE.appendChild(document.createTextNode(CSS + CSS_API_PLATFORM_SITE));
 document.querySelector('body').appendChild(STYLE);
