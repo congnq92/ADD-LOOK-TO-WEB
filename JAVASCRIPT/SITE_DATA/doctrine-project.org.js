843f27e657be2a9f12b05575931ea6ba9ecd0ef6
Cong NQ NexleSoft <congnq@nexlesoft.com>
2022-07-25 16:20:09 +0700

    add doctrine-project.org

---
+++ JAVASCRIPT/SITE_DATA/doctrine-project.org.js
@@ -0,0 +1,24 @@
+DATA = [
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'body',
+        CLASS_LIST_BY_SPACE: 'UPPER w3-white w3-text-black'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: 'code,.console,.code-block-table',
+        CLASS_LIST_BY_SPACE: 'CONG_CODE'
+    },
+    {
+        TYPE: 'ADD_CLASS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.doctrine-navbar',
+        CLASS_LIST_BY_SPACE: 'HIDE'
+    },
+    {
+        TYPE: 'ADD_CSS',
+        SELECTOR_CSS_LIST_BY_COMMA: '.container-wrapper',
+        CSS_TEXT: 'max-width: 100%;'
+    },
+    //
+    {EMPTY_LINE: 'EMPTY_LINE'}
+];
